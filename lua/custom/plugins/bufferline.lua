90acb2e9b523492fd59a83217cbfcc88ddb84544
Angelo Pengue <apengue1@gmail.com>
2024-01-09 22:17:11 -0500

    Adds buffer line motions

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -20,6 +20,30 @@ return {
     config = function(_, opts)
       vim.opt.termguicolors = true
       require("bufferline").setup(opts)
+
+      local next_buffer = function()
+        vim.cmd(':bnext')
+      end
+
+      local previous_buffer = function()
+        vim.cmd(':bprev')
+      end
+
+      local close_current_buffer = function()
+        vim.cmd(':bdelete')
+        vim.cmd(':bfirst')
+      end
+
+      local close_all_buffers = function()
+        vim.cmd(':BufferLineCloseOthers')
+        vim.cmd(':bdelete')
+      end
+
+      vim.keymap.set('n', '<A-[>', previous_buffer, { desc = 'Previous buffer' })
+      vim.keymap.set('n', '<A-]>', next_buffer, { desc = 'Next buffer' })
+      vim.keymap.set('n', '<A-\\>', close_current_buffer, { desc = 'Close buffer' })
+      vim.keymap.set('n', '<A-BS>', close_all_buffers, { desc = 'Close all buffers' })
     end
   }
 }
+
