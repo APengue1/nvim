215b6136c054711e05dc0abe276b2ad8e2a4674b
Angelo Pengue <apengue1@gmail.com>
2024-01-09 20:49:37 -0500

    Uses relative line in lualine

---
+++ lua/custom/plugins/statusline.lua
@@ -0,0 +1,26 @@
+  return {
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'tokyonight',
+        component_separators = '|',
+        section_separators = '',
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {
+          {
+            'filename',
+            path = 1, -- relative to working root
+          }
+        },
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+    },
+  }
+
