bfda7af49f34239b534544ed3e35c923be10047b
Angelo Pengue <apengue1@gmail.com>
2024-01-11 23:10:14 -0500

    Fix the breadcrumbs

--- lua/custom/plugins/breadcrumbs.lua
+++ lua/custom/plugins/breadcrumbs.lua
@@ -1,9 +1,26 @@
 return {
   {
+    "SmiteshP/nvim-navic",
+    opts = {
+      lsp = {
+        auto_attach = true,
+      },
+    },
+    config = function(_, opts)
+      -- Not sure why we have to explicitly call setup on this function.
+      require("nvim-navic").setup(opts)
+    end,
+  },
+  {
     "LunarVim/breadcrumbs.nvim",
     dependencies = {
         {"SmiteshP/nvim-navic"},
     },
+    config = function()
+      -- Not sure why we have to explicitly call setup on this function.
+      -- It also does not take an opts parameter.
+      require("breadcrumbs").setup()
+    end,
   },
 }
 
