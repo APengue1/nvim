3bb5a356bfa0c1966e58aaeacf32fac530ac6df2
Angelo Pengue <apengue1@gmail.com>
2024-01-20 16:48:34 -0500

    Update vim options

--- lua/custom/settings/options.lua
+++ lua/custom/settings/options.lua
@@ -1,3 +1,15 @@
 vim.wo.relativenumber = true
-vim.o.hlsearch = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
 
