757e81c4b84bceaa58cf5b782b6e619e0cc9e401
Angelo Pengue <apengue1@gmail.com>
2024-02-18 10:12:13 -0500

    Makes default Telescope preview vertical

--- lua/telescope-setup.lua
+++ lua/telescope-setup.lua
@@ -2,6 +2,12 @@
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    layout_strategy = "vertical",
+    layout_config = {
+      vertical = {
+        preview_cutoff = 0,
+      },
+    },
     mappings = {
       i = {
         ['<C-u>'] = false,
