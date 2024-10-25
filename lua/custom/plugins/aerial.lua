c35f8917b4a24ab2200a6034d220c283915dd24b
Angelo Pengue <apengue1@gmail.com>
2024-10-25 19:26:36 -0400

    Updates aerial to 60a784614acb1d7695bd9ae0fee8ada1bf7b0c28 (#2)

--- lua/custom/plugins/aerial.lua
+++ lua/custom/plugins/aerial.lua
@@ -1,7 +1,13 @@
 return {
   {
     'stevearc/aerial.nvim',
-    opts = {},
+    opts = {
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons"
@@ -12,8 +18,17 @@ return {
       require('which-key').register {
         ['<leader>a'] = { name = '[A]erial', _ = 'which_key_ignore' },
       }
+
       vim.keymap.set("n", "<leader>at", "<cmd>AerialToggle!<CR>",
         { desc = '[A]erial [T]oggle' }
+      )
+
+      vim.keymap.set("n", "<leader>an", "<cmd>AerialNavToggle<CR>",
+        { desc = '[A]erial [N]av Toggle' }
+      )
+
+      vim.keymap.set("n", "<leader>ag", "<cmd>AerialGo<CR>",
+        { desc = '[A]erial [G]o' }
       )
     end
   }
